8631b60d9d25edc0e25746d9a94874f82fb01206
dam <jdurancruz@danielcastelao.org>
2025-09-12 14:16:51 +0200

    Ej 3 parte2

--- Tarea1/src/Parte2.java
+++ Tarea1/src/Parte2.java
@@ -6,5 +6,9 @@ public class Parte2 {
 
         //Ej2
         Parte1.creaFicheiro("/home/dam/Documentos/AcesoDatos/Tarea1/arquivosdir","Products.txt");
+
+        //Ej3
+        Parte1.creaDirectorio("/home/dam/Documentos/AcesoDatos/Tarea1/arquivosdir/subdir");
+        Parte1.creaFicheiro("/home/dam/Documentos/AcesoDatos/Tarea1/arquivosdir/subdir","Products2.txt");
     }
 }
