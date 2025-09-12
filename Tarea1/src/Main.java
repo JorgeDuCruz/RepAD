03c73736dda90914801842d0d92f3dde120ef97d
dam <jdurancruz@danielcastelao.org>
2025-09-12 12:55:48 +0200

    Ej 4 hecho

--- Tarea1/src/Main.java
+++ Tarea1/src/Main.java
@@ -1,9 +1,12 @@
 import java.io.File;
+import java.io.IOException;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println(eDirectorio("/home/dam/Documentos/AcesoDatos"));
         System.out.println(eFicheiro("/home/dam/Documentos/AcesoDatos"));
         creaDirectorio("/home/dam/Documentos/AcesoDatos/Tarea1/pruebaDirectorio");
+        creaFicheiro("/home/dam/Documentos/AcesoDatos/Tarea1/pruebaDirectorio","Prueba");
     }//en static
 
 
@@ -13,18 +16,14 @@ public class Main{
     }
     public static boolean eFicheiro(String cadea){
         File archivo = new File(cadea);
-        if(archivo.isFile()){
-            return true;
-        }else{
-            return false;
-        }
+        return archivo.isFile();
 
     }
     public static void creaDirectorio(String cadea){
         File archivo = new File(cadea);
         boolean creado = archivo.mkdir();
         if(creado){
-            System.out.printf("Archivo creado");
+            System.out.println("Archivo creado");
         }else{
             System.out.println("No se ha creado");
         }
@@ -32,7 +31,20 @@ public class Main{
 
 
     public static void creaFicheiro(String dirName, String fileName){
-        File archivo = new File(dirName);
+        File archivo = new File(dirName,fileName);
+        boolean resultado;
+        try{
+            resultado=archivo.createNewFile();
+            if (resultado) {
+                System.out.println("Archivo creado");
+            }else {
+                System.out.println("Archivo no creado");
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+
+        }
+
 
     }
 }//end class
