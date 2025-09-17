3853678d6e90286c9a9cde1ae590ce7483dffca7
dam <jdurancruz@danielcastelao.org>
2025-09-17 14:10:51 +0200

    Parte 1 apartado 3 hecho

--- Tarea2/src/Main.java
+++ Tarea2/src/Main.java
@@ -4,10 +4,11 @@ import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        crearArchivo("/home/dam/Documentos/AD/RepAD/Tarea2/src/texto1.txt","src/texto2.txt");
+        copiarArchivo("/home/dam/Documentos/AD/RepAD/Tarea2/src/texto1.txt","src/texto2.txt");
+        anhadirArchivo("/home/dam/Documentos/AD/RepAD/Tarea2/src/texto1.txt","src/texto2.txt");
     }
 
-    public static void crearArchivo(String ruta,String rutaE){
+    public static void copiarArchivo(String ruta,String rutaE){
         try {
             FileInputStream leer = new FileInputStream(new File(ruta));
             FileOutputStream escribir = new FileOutputStream(new File(rutaE));
@@ -26,6 +27,29 @@ public class Main {
 
 
         }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void anhadirArchivo(String ruta,String rutaE) {
+        try {
+            FileInputStream leer = new FileInputStream(new File(ruta));
+            FileOutputStream escribir = new FileOutputStream(new File(rutaE),true);
+            int lectura, i = 0;
+            byte[] b = new byte[leer.available()];
+
+            while (true) {
+                lectura = leer.read();
+                if (lectura == -1) {
+                    break;
+                }
+                b[i] = (byte) lectura;
+                i++;
+            }
+            escribir.write(b);
+
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
