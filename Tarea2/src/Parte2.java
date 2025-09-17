f9950acc46ede7e160f2f6840b5ee37a5b21d8af
dam <jdurancruz@danielcastelao.org>
2025-09-17 14:49:36 +0200

    Parte 2 apartado 3 empezado

--- Tarea2/src/Parte2.java
+++ Tarea2/src/Parte2.java
@@ -1,3 +1,8 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Parte2 {
     public static void main(String[] args) {
         Parte1.copiarArchivo("TobyFox.jpg","foto2.jpg");
@@ -5,5 +10,15 @@ public class Parte2 {
 
         Parte1.anhadirArchivo("TobyFox.jpg","foto2.jpg");
         //Apartado 2 : No se ve duplicada la imagen pero si se percibe que ocupa el doble
+    }
+
+
+    public static void copiarBuffered(String rutaO, String rutaD){
+        try {
+            BufferedInputStream leer = new BufferedInputStream(new FileInputStream(rutaO));
+            byte [] b = leer.readAllBytes();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
