12aa34ebb8495f8fae513c69fe61d6beeedef5dd
dam <jdurancruz@danielcastelao.org>
2025-09-19 12:40:48 +0200

    Parte 2 apartado 3 y 4 Hechos. Apartado 3 no esta igual que en la parte 1, puede que necesite hacer más parecidos los metodos

--- Tarea2/src/Parte2.java
+++ Tarea2/src/Parte2.java
@@ -1,15 +1,19 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Parte2 {
     public static void main(String[] args) {
-        Parte1.copiarArchivo("TobyFox.jpg","foto2.jpg");
+        String rutaFoto1 = "TobyFox.jpg",rutaFoto2 = "foto2.jpg",rutaFoto2B = "fotoBuffered.jpg";
+
+        Parte1.copiarArchivo(rutaFoto1,rutaFoto2);
         //Apartado 1: Si ocupan lo mismo
 
-        Parte1.anhadirArchivo("TobyFox.jpg","foto2.jpg");
-        //Apartado 2 : No se ve duplicada la imagen pero si se percibe que ocupa el doble
+        Parte1.anhadirArchivo(rutaFoto1,rutaFoto2);
+        //Apartado 2: No se ve duplicada la imagen pero si se percibe que ocupa el doble
+
+        //Apartado 3:
+        copiarBuffered(rutaFoto1,rutaFoto2B);
+
+        //Apartado 4: No noto diferencias de copiado porque el copidado ya era muy rapido sin los buffered
     }
 
 
@@ -17,6 +21,8 @@ public class Parte2 {
         try {
             BufferedInputStream leer = new BufferedInputStream(new FileInputStream(rutaO));
             byte [] b = leer.readAllBytes();
+            BufferedOutputStream escribir = new BufferedOutputStream(new FileOutputStream(rutaD));
+            escribir.write(b);
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
