f2c30f42fe49a0d20e38ea3a908beb1c462f8ea1
dam <jdurancruz@danielcastelao.org>
2025-09-26 14:04:55 +0200

    Configuracion de la parte 3 hecha en la main

--- Tarea2basedatos/src/Main.java
+++ Tarea2basedatos/src/Main.java
@@ -3,11 +3,12 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
         Connection conn = Conexion.conexion();
-        //Anime prueba = new Anime("Naruto", "Anime de ninjas", "2002-10-03", 85);
-        //ServicioAnime.insertarDatos(prueba,conn);
-        //ServicioAnime.borrarDatos("Naruto",conn);
-        //ServicioAnime.leer(conn);
+        Anime prueba = new Anime("Naruto", "Anime de ninjas", "2002-10-03", 85);
+        ServicioAnime.insertarDatos(prueba,conn);
+        ServicioAnime.leer(conn);
         ServicioAnime.actualizarDatos(conn,"Naruto","Naruto","Ciclos de Odio","2022-12-11",95);
         ServicioAnime.leerFiltros(conn,"Naruto");
+        ServicioAnime.borrarDatos("Naruto",conn);
+        ServicioAnime.leer(conn);
     }
 }
